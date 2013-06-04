39e0206125aeedc0340ef0a8212c16ae4fa0851a
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-06-04 12:56:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaysOfImpact::Application.config.secret_token = '9ae809c889dda00192b3e0ad53c599abf501dbe96c42c8a33bb10600994b4f59ac9ce3c4bd812658658fc999992baaa682a70032aff58c2d878c803e34499739'
