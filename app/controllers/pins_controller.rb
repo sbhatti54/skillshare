1e57a0cd5cf09663ce024aeb48565b6fbe7e3c2e
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-06-05 15:31:32 -0500

    delete

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
+    @pins = Pin.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
