4100e22b44d0d74e2b25164e5007d5e9d7061a46
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-06-05 13:35:33 -0500

    edited app.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ DaysOfImpact::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   # In production, :host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { :host => 'daysofimpact.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 
 end
