3099e6f2349dfd181c62f68c6eb32ab097214243
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-10-04 14:22:36 -0500

    roi

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,8 +22,21 @@ class PagesController < ApplicationController
   end
 
   def calculate
-  	@cost = params[:cost].to_f
-  	@rev = params[:revenue].to_f	
+  	@email_cost_of_campaign = params[:ecoc].to_f
+  	@cost_per_redemption = params[:cpr].to_f
+  	@email_number_of_redemptions = params[:red].to_f	
+  	@revenue_per_redemption = params[:rev].to_f
+  	@email_total_cost = @email_cost_of_campaign + (@email_number_of_redemptions * @cost_per_redemption)
+  	@email_total_revenue = @revenue_per_redemption * @email_number_of_redemptions
+  	@email_profit = @email_total_revenue - @email_total_cost
+  	@email_roi = 100 * (@email_profit) / @email_total_cost	
+    @wallet_manager_cost_of_campaign = params[:wmcoc].to_f
+    @wallet_manager_number_of_redemptions = @email_number_of_redemptions * 1.07
+    @wallet_manager_total_cost = @wallet_manager_cost_of_campaign + (@wallet_manager_number_of_redemptions * @cost_per_redemption)
+    @wallet_manager_total_revenue = @revenue_per_redemption * @wallet_manager_number_of_redemptions
+    @wallet_manager_profit = @wallet_manager_total_revenue - @wallet_manager_total_cost
+    @wallet_manager_roi =100 * @wallet_manager_profit / @wallet_manager_total_cost
+
   	render :layout => "calculate" 
   end
 end
